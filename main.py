fb79a121bc29774f40db0a300b773d3c32fef987
Aruanangshu Biswas <20253878+dotslash21@users.noreply.github.com>
2018-05-08 15:38:24 +0530

    Fixed Bugs

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ def user_login():
     password = input("Enter the password: ")
 
     try:
-        response = json.loads(myVoiceIt.getUser(developerId, uid))
+        response = json.loads(myVoiceIt.getUser(uid, password))
+        print(response)
 
-        if response["Result"] == "Success":
+        if response["ResponseCode"] == "SUC":
             wav_path = input("Enter the location of wav file: ")
             response = json.loads(myVoiceIt.authentication(uid, password, wav_path, locale))
+            print(response)
 
-            if response["Result"] == "Success":
+            if response["ResponseCode"] == "SUC":
                 userAuthenticated = True
                 userName = uid
 
@@ -37,14 +39,30 @@ def create_user():
     try:
         response = json.loads(myVoiceIt.getUser(uid, password))
 
-        if response["Result"] == "User not found":
-            '''
-            TO-DO
-            '''
+        if response["ResponseCode"] == "UNF":
+            response = json.loads(myVoiceIt.createUser(uid, password))
+
+            if response["ResponseCode"] != "SUC":
+                print("Error creating new user!")
 
     except (ValueError, KeyError, TypeError):
         print("JSON format error")
 
+    print("User", uid, ",is created sucesfully!")
+
+    # create enrollment
+    print("You will now need to upload some voice samples for voice authentication.")
+    print("You will be prompted 3 times for voice enrollment")
+    for i in range(3):
+        wav_path = str(input("Enter the .wav file for upload: "))
+        response = json.loads(myVoiceIt.createEnrollment(uid, password, wav_path, locale))
+        print(response)
+        if response["ResponseCode"] != "SUC":
+            print("Enrollment failed try again")
+            i -= 1
+
+    print("New user registration complete!")
+    
 
 if __name__ == "__main__":
     print("------------------------------------------------------")
@@ -52,7 +70,7 @@ if __name__ == "__main__":
     print("------------------------------------------------------")
     print("1 - Login")
     print("2 - New user registration.")
-    choice = input("Your choice: ")
+    choice = int(input("Your choice: "))
     print("------------------------------------------------------")
 
     if choice == 1:
